d7265aad0f4791f55514b18692913ed6054a39ea
omarrui <rouiguio919@gmail.com>
2025-05-14 14:15:57 +0200

    task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bash/python3
+def no_c(my_string):
+    new_str = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            continue
+        new_str += i
+    return new_str
