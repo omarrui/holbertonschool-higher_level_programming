627f963bbbf8681fb49dfa98ffe7361ede6f5f26
omarrui <rouiguio919@gmail.com>
2025-05-14 13:52:59 +0200

    fixing spacing issue task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list1 = my_list[:]
-    if idx < 0 and idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return list1
     list1[idx] = element
     return list1
