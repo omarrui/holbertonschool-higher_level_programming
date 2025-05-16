0e933c5d1842dfd4eeed665718ab0f8d50bfaa5d
omarrui <rouiguio919@gmail.com>
2025-05-16 17:54:04 +0200

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,35 +2,16 @@
 """
 add_integer.py
 
-This module defines a function that adds two integers or floats.
-Floats are cast to integers before addition.
+this module add 2 numbers
 """
 
-import math
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats (which are cast to ints).
-    
-    Args:
-        a: The first number (int or float).
-        b: The second number (int or float), default is 98.
-    
-    Returns:
-        The sum of a and b as an integer.
-
-    Raises:
-        TypeError: If a or b are not int or float.
-        ValueError: If a or b are NaN or infinity.
+    that adds 2 integer, transform float in to integer and handle wrong type
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    if isinstance(a, float) and (math.isnan(a) or math.isinf(a)):
-        raise ValueError("cannot convert float NaN or infinity to integer")
-    if isinstance(b, float) and (math.isnan(b) or math.isinf(b)):
-        raise ValueError("cannot convert float NaN or infinity to integer")
-
     return int(a) + int(b)
