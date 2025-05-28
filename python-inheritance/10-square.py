17163db6bb11bdc4b88c8bb0e807fdef57823661
omarrui <rouiguio919@gmail.com>
2025-05-28 15:45:05 +0200

    task10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+square
+
+this file contains the class Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    this is a class Square with
+    the size of square
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
