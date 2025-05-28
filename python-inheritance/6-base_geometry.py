081c9eee5997873edcf39d50abaaab3bc7f919d3
omarrui <rouiguio919@gmail.com>
2025-05-28 09:27:14 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class with an area method
+"""
+
+class BaseGeometry:
+    """Class with area method that is not implemented yet"""
+    def area(self):
+        raise Exception("area() is not implemented")
