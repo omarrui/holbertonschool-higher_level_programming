d97f3b3008d0199db89e352165033eff8e57ea8c
omarrui <rouiguio919@gmail.com>
2025-06-02 20:29:37 +0200

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """writes a string into txt file and returns the number of ch written"""
+    with open(filename, "w", encoding="UTF8") as f:
+        return f.write(text)
