67859764c38f770b81c10be60a3d37b356c2e148
omarrui <rouiguio919@gmail.com>
2025-06-04 00:13:37 +0200

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+read_file
+
+This module contains a function that reads and prints a file's content.
+"""
+
 def read_file(filename=""):
-    """reads a text to a file and  prints its contents to stdout"""
+    """Reads a text file and prints its contents to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
