fbf573b72db4d680ea2550273cd11d3b338cb841
omarrui <rouiguio919@gmail.com>
2025-06-04 15:04:00 +0200

    tasl 5 fixing the file name

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-5save to json
+save_to_json_file
 
 writes an object to text file
 """
-
-
 import json
 
 
@@ -14,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     writes an object to a text dile
     """
     with open(filename, "w") as f:
-        return json.dump(my_obj)
+        f.write(json.dumps(my_obj))
