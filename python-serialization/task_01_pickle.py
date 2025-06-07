55e2b7e6f2d5e67261f5584763a11b8c53a15c0f
omarrui <rouiguio919@gmail.com>
2025-06-07 19:00:36 +0200

    task 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+"""
+seriailize and deserialize using pickle
+
+this module has 2 functions
+"""
+import pickle
+
+
+class Custoom0bject:
+    """
+    this class contains the display
+    serialize and deserialize methods
+    """
+    def __innit__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}\nAge: {}\nIs Student: {}".format
+              (self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        if not filename:
+            return None
+        try:
+            with open(filename, "wb")as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        if not filename:
+            return None
+        try:
+            with open(filename, "rb")as f:
+                return pickle.load(f)
+        except exception:
+            return None
