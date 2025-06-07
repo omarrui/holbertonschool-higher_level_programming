edc93b535eec8b04af44b94cf3e761cf3610f8ae
omarrui <rouiguio919@gmail.com>
2025-06-07 16:10:04 +0200

    task 0 adding the read

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,3 +9,15 @@ def serialize_nd_save_to_file(data, filename):
     """
     with open(filename, 'w') as file:
         json.dump(data, file)
+    pass
+
+def load_from_file_and_deserialize(filename):
+    """
+    Load data from a JSON file and deserialize it.
+    
+    :param filename: Name of the file to load the serialized data from
+    :return: Deserialized data
+    """
+    with open (filename, 'r') as file:
+        data = json.load(file)
+    pass
