6e535b17068607f65a28f19d2cb3f178f1077424
omarrui <rouiguio919@gmail.com>
2025-06-07 16:47:49 +0200

    task 0 adding the comments that r missing

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""
+basic_serialization
+
+this module contains 2 functions
+to serialize and deserialize
+"""
 import json
 def serialize_nd_save_to_file(data, filename):
     """
     Serialize data to a JSON file.
-    
-    :param data: Data to serialize (can be a dict, list, etc.)
-    :param filename: Name of the file to save the serialized data
     """
     with open(filename, 'w') as file:
         json.dump(data, file)
@@ -14,9 +17,6 @@ def serialize_nd_save_to_file(data, filename):
 def load_from_file_and_deserialize(filename):
     """
     Load data from a JSON file and deserialize it.
-    
-    :param filename: Name of the file to load the serialized data from
-    :return: Deserialized data
     """
     with open (filename, 'r') as file:
         data = json.load(file)
