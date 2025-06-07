d8c03ed4d86dc22ec3b13844065b030f5992f7b2
omarrui <rouiguio919@gmail.com>
2025-06-07 22:53:13 +0200

    task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this module contains 1 function
+convert_csv_to_json and import 2 modules
+csv and json
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """
+    converts cqv file to json file
+    """
+    try:
+        with open(filename, "r") as file:
+            csv_reader = csv.DictReader(file)
+            data = []
+            for row in csv_reader:
+                data.append(dict(row))
+        with open("fata.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
+        return True
+    except fileNotFoundError:
+        return False
