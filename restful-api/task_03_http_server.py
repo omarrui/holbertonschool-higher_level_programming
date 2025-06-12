ef1455ae3584a25ba95ca470af0e9bd42cf547b1
omarrui <rouiguio919@gmail.com>
2025-06-12 17:24:12 +0200

    tasK3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,7 +16,33 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
     special path
     """
     def do_GET(self):
-        
+        if self.path == "/":
+            self.send_response(200)
+            self.send_headeer("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "applocation/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_str = json.dumps(data)
+            self.wfile.write(json_str.encode())
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("content-type", "application/json")
+            self.end_headers()
+            data = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            json_str = json.dumps(data)
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
 
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
