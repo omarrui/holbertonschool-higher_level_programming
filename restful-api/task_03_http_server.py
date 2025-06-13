074bf41576266f7533428b40a06a4018070f4eb7
omarrui <rouiguio919@gmail.com>
2025-06-13 09:18:52 +0200

    task3 fixing a few typo errors

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,25 +18,24 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_headeer("Content-type", "text/plain")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
             self.send_response(200)
-            self.send_header("Content-type", "applocation/json")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             json_str = json.dumps(data)
             self.wfile.write(json_str.encode())
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("content-type", "application/json")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-            }
+            data = {"version": "1.0",
+                    "description": "A simple API built with http.server"}
             json_str = json.dumps(data)
+            self.wfile.write(json_str.encode())
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
@@ -45,7 +44,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
 
 PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+Handler = MyHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
