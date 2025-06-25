a33f2e30caaa352586e1dd562723321770965696
omarrui <rouiguio919@gmail.com>
2025-06-25 14:53:11 +0200

    task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- creating a full database in hbtn
+-- and add a few rows
+create table if not exists second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+)
+INSERT into second_table(is, name, score) 
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3,'Bob', 14),
+(4, 'George', 8),
