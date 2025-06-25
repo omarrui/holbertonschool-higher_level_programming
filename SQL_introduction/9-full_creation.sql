e47ea73e462ed7f0f2cdffc2d98b9e1cf275be16
omarrui <rouiguio919@gmail.com>
2025-06-25 14:54:32 +0200

    task 9 adding the missing semi colon

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,10 +4,10 @@ create table if not exists second_table(
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
 INSERT into second_table(is, name, score) 
 VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3,'Bob', 14),
-(4, 'George', 8),
+(4, 'George', 8);
