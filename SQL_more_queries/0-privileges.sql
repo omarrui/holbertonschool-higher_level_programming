c37cc7df61c734ba0e3b69d3ae10fadcb87a3e9b
omarrui <rouiguio919@gmail.com>
2025-06-25 19:17:16 +0200

    tazsk 0 adding caps

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- list all priviliges for user_0d_1 & user_0d_2
 --- mysql users user 1 and user 2
-SHOW GRANTS for user_0d_1@'localhost';
-SHOW GRANTS for user_0d_2@'localhost';
+SHOW GRANTS FOR user_0d_1@'localhost';
+SHOW GRANTS FOR user_0d_2@'localhost';
