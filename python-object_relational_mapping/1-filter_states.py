d5332fe69b6f510a037570bbfa9891f6a6dbafdc
omarrui <rouiguio919@gmail.com>
2025-06-27 18:26:21 +0200

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,56 +1,23 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Module that lists all states from the database hbtn_0e_0_usa
+"""
 import sys
-"""
-1-filter_states
-lists all states from the
-database hbtn_0e_0_usa
-where name starts with 'n'
-and is ordered by id (ascending)
-"""
+import MySQLdb
 
 
 if __name__ == "__main__":
-	db_connection = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=sys.argv[1],
-		passwd=sys.argv[2],
-		db=sys.argv[3]
-	)
-	cursor = db_connection.cursor()
-	cursor.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id;")
-	rows = cursor.fetchall()
-	for row in rows:
-		print(row)
-	cursor.close()
-	db_connection.close()
-#!/usr/bin/python3
-import MySQLdb
-import sys
-"""
-1-filter_states
-that lists all states from the
-database hbtn_0e_0_usa
-where name starts with 'n'
-and is ordered by id (ascending)
-"""
-
-
-if __name__ == "__main__":
-	db_connection = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=sys.argv[1],
-		passwd=sys.argv[2],
-		db=sys.argv[3]
-	)
-	name_search = sys.argv[4]
-	cursor = db_connection.cursor()
-	query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(name_search))
-	cursor.execute(query)
-	rows = cursor.fetchall()
-	for row in rows:
-		print(row)
-	cursor.close()
-	db_connection.close()
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%'")
+    for state in cursor.fetchall():
+        print(state)
