4812039d2c214d2fe3503d9c6a1866514d54be6d
omarrui <rouiguio919@gmail.com>
2025-06-27 21:51:16 +0200

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-Module that lists all states from the database hbtn_0e_0_usa
+1-filter_states
+that lists all states from the
+database hbtn_0e_0_usa
+where name starts with 'n'
+and is ordered by id (ascending)
 """
-import sys
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    db_connection = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-
-    cursor = db.cursor()
-
-    query = ("SELECT * FROM states "
-             "WHERE CONVERT(name USING Latin1) "
-             "COLLATE Latin1_General_CS LIKE 'N%'")
-    cursor.execute(query)
-
-    for state in cursor.fetchall():
-        print(state)
-
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     cursor.close()
-    db.close()
+    db_connection.close()
