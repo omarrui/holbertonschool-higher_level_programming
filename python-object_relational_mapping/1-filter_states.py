e57158022a7e15341044a35dbf96e3789e1fd96c
omarrui <rouiguio919@gmail.com>
2025-06-27 19:18:26 +0200

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,17 +7,25 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%'")
-    for state in cursor.fetchall():
-        print(state)
+    try:
+        # Connect to the database
+        db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+        cursor = db.cursor()
+
+        # Execute the query to fetch states starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        for state in cursor.fetchall():
+            print(state)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
