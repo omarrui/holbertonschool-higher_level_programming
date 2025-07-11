75dd1e675c2955b2a6e87fb2b4114325fa82cedd
omarrui <rouiguio919@gmail.com>
2025-07-11 21:54:37 +0200

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
 
-const args = process.argv.slice(2).map(Number);
-if (args.length < 2) {
+if (argv[3] === undefined) {
   console.log(0);
 } else {
-  const unique = [...new Set(args)].sort((a, b) => b - a);
-  console.log(unique[1]);
+  let i = 2;
+  let one = 0;
+  let seconde = 0;
+  while (i < argv.length) {
+    if (one < Number(argv[i])) {
+      seconde = one;
+      one = Number(argv[i]);
+    }
+    if (seconde < Number(argv[i]) && Number(argv[i]) < one) {
+      seconde = Number(argv[i]);
+    }
+    i++;
+  }
+  console.log(seconde);
 }
