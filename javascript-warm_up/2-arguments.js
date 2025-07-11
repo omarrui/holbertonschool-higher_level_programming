f5a4a7488f1cc24f199362677b1211e36a1cee29
omarrui <rouiguio919@gmail.com>
2025-07-11 23:09:32 +0200

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const{ argv } = require('node:process');
+const { argv } = require('node:process');
 
 if (argv.length >= 4) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 } else if (argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
