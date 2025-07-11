15ef534110bf525fafa3cd50e16e247ddb3a730e
omarrui <rouiguio919@gmail.com>
2025-07-11 23:08:44 +0200

    task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const C = 'C is fun';
-const Python = ' Python is cool';
+const Python = 'Python is cool';
 const js = 'JavaScript is amazing';
 console.log(C + '\n' + Python + '\n' + js);
