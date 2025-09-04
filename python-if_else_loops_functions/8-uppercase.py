da274d5d5b04da7c7b755c4fc9ee7c52f9b98fd2
omarrui <rouiguio919@gmail.com>
2025-09-04 14:03:45 +0200

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:  # 'a' to 'z'
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
