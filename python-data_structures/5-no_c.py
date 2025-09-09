174a06f59304af7729d2bcd9c11541b7c1df823f
omarrui <rouiguio919@gmail.com>
2025-09-09 21:12:19 +0200

    tasks

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_str = ""
 
     for ch in my_string:
-        if ch != 'c' and ch != 'C':   
-           new_str += ch
+        if ch != 'c' and ch != 'C':
+            new_str += ch
     return new_str
