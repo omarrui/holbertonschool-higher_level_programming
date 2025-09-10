b00b554c84a3b7aceb31f651c716088ccac669dc
omarrui <rouiguio919@gmail.com>
2025-09-10 16:53:41 +0200

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=None):
     if my_list is None or len(my_list) == 0:
         return None
-    
+
     max_val = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > max_val:
             max_val = my_list[i]
-    
+
     return max_val
