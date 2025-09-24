3344d102ebc071a9b017cfd261d15cea2b515a23
omarrui <rouiguio919@gmail.com>
2025-09-24 18:30:30 +0200

    tasks

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""built-in Python function that returns a list of all attributes"""
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return dir(obj)
